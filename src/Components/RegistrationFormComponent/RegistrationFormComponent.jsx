9039f33e7c892e8ae13486389d2c4cf5a1dfc048
pavel <pal5555@mail.ru>
2023-12-08 20:57:17 +0300

    'validation-registration_form'

--- src/Components/RegistrationFormComponent/RegistrationFormComponent.jsx
+++ src/Components/RegistrationFormComponent/RegistrationFormComponent.jsx
@@ -1,4 +1,5 @@
 import  react, { useState } from 'react'
+import { useForm} from "react-hook-form"
 import { useInput } from '../../hooks/useInput'
 import { Link } from 'react-router-dom'
 
@@ -11,48 +12,73 @@ export const RegistrationFormComponent = ()=>{
         const login = useInput('')
         const password = useInput('')
         const confirmPassword = useInput('')
-        const [gender, setGender] = useState('')
+        const [gender, setGender] = useState('Man')
 
         const changeGender= (event)=>{
             setGender(event.target.value)
         }
+        const {
+            register,
+            formState: {errors},
+            handleSubmit,
+            watch
+            
+        } =  useForm()
   
-        
+        const onSubmit=( data)=>{
+              
+                console.log(JSON.stringify(data))
+        }
 
 
 
         return(
-                <form action="form" className="form">
+                <form action="form" className="form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="form__wrapper">
                     <div className="form__title">Регистрация</div>
                     <div className="form__label">
                     <label htmlFor="Email" className="form-label">Имя профиля</label>
                     </div>
                     <div className="form__input">
-                        <input type="text" value={name.value} onChange={name.onChange} className="form-input" placeholder='Имя' />
+                        <input
+                        {...register('profileName',{
+                            required: 'Поле обязательное !'
+                        })} 
+                        type="text" 
+                       // value={name.value} 
+                       // onChange={name.onChange} 
+                        className="form-input" 
+                        placeholder='Имя' 
+                        
+                        />
+                      
+                       {errors?.profileName && <p className='form__error'>{errors?.profileName.message}</p>}
                     </div>
                     <div className="form__label">
                     <label htmlFor="Email" className="form-label">Дата рождения</label>
                     </div>
                     <div className="form__group">
                      <div className="form__select">
-                        <select name="day" value={day.value} onChange={day.onChange}>
-                        <option value="День">День</option>
+                        <select name="day" 
+                        {...register('day')} >
+                       
                         <option value="1">1</option>
                         <option value="2">2</option>
                          </select>
                         </div>   
                         <div className="form__select">
-                        <select name="month" value={month.value} onChange={month.onChange}>
-                        <option value="Месяц">Месяц</option>
+                        <select name="month" 
+                         {...register('month')}
+                        >
                         <option value="Январь">Январь</option>
                         <option value="Февраль">Февраль</option>
                          </select>
                         </div>   
 
                         <div className="form__select">
-                        <select name="year" value={year.value} onChange={year.onChange}>
-                        <option value="Год">Год</option>
+                        <select name="year"
+                         {...register('year')}
+                        >
                         <option value="1997">1997</option>
                         <option value="1998">1998</option>
                         </select>
@@ -62,16 +88,34 @@ export const RegistrationFormComponent = ()=>{
                         <div className="genders">
                             <div className="genders__items">
                                 <div className="gender__item">
-                                    <input type="radio" value='Man' className="gender__radio" name='gender' onChange={changeGender}/>
+                                    <input 
+                                    type="radio" 
+                                    value='Man' 
+                                    className="gender__radio" 
+                                    name='gender' 
+                                   // onChange={changeGender}
+                                   {...register('gender',{
+                                    required: 'Поле обязательное !'
+                                   })}
+                                    />
                                     <span className="gender__text">Мужчина</span>
                                     
                                 </div>
                                 <div className="gender__item">
-                                    <input type="radio"  value='Wooman' className="gender__radio" name='gender' onChange={changeGender} />
+                                    <input 
+                                    type="radio"  
+                                    value='Wooman' 
+                                    className="gender__radio" 
+                                    name='gender' 
+                                   // onChange={changeGender} 
+                                   {...register('gender', {
+                                    required: 'Поле обязательное !'
+                                   })}
+                                    />
                                     <span className="gender__text">Женщина</span>
                                 </div>
-                                {gender}
                             </div>
+                            {errors?.gender && <p className='form__error'>{errors?.gender.message}</p>}
                         </div>
                     </div>
                     
@@ -79,23 +123,72 @@ export const RegistrationFormComponent = ()=>{
                     <label htmlFor="Email" className="form-label">Логин</label>
                     </div>
                     <div className="form__input">
-                        <input type="text" value={login.value} onChange={login.onChange} className="form-input" placeholder='Логин' />
+                        <input 
+                        type="text" 
+                       // value={login.value} 
+                       // onChange={login.onChange} 
+                        className="form-input" 
+                        placeholder='Логин' 
+                        {...register('login',{
+                            required: 'Поле обезательное !',
+                            pattern:{
+                                value:/^[а-яА-ЯёЁa-zA-Z0-9]+$/,
+                                message: 'Должны быть только буквы и цыфры !'
+                            }
+                        })}
+                        />
                     </div>
+                    {errors?.login && <p className='form__error'>{errors?.login.message}</p>}
                     <div className="form__label">
                     <label htmlFor="Password" className="form-label">Пароль</label>
                     </div>
                     <div className="form__input">
-                        <input type="password"  value={password.value} onChange={password.onChange} className="form-input" placeholder='Пароль' />
+                        <input 
+                        type="password"  
+                       // value={password.value} 
+                        //onChange={password.onChange} 
+                        className="form-input" 
+                        placeholder='Пароль' 
+                        {...register('password',{
+                            required:'Поле обязательное !',
+                            minLength:{
+                                value: 6,
+                                message: 'Минимум 6 символов !'
+                            }
+                        })} 
+                        
+                        />
+                           {errors?.password && <p className='form__error'>{errors?.password.message}</p>}
                     </div>
                     <div className="form__label">
                     <label htmlFor="Password" className="form-label">Повторите пароль</label>
                     </div>
                     <div className="form__input">
-                        <input type="password"  value={confirmPassword.value} onChange={confirmPassword.onChange} className="form-input" placeholder='Пароль' />
+                        <input 
+                        type="password"  
+                        //value={confirmPassword.value} 
+                        //onChange={confirmPassword.onChange} 
+                        className="form-input" 
+                        placeholder='Пароль' 
+                        {...register('confirmPassword',{
+                            required:'Поле обязательное !',
+                            minLength:{
+                                value: 6,
+                                message: 'Минимум 6 символов !',
+                                },
+                                validate: (val) => {
+                                    if (watch('password') != val) {
+                                        return "Пароль не совпадает !";
+                                      }
+                                  },  
+                        })}
+                       
+                        />
+                         {errors?.confirmPassword && <p className='form__error'>{errors?.confirmPassword.message}</p>}
                     </div>
     
                     <div className="form__success">
-                        <button  type='button' className='button'>Регистрироватся</button>
+                        <button  type='submit' className='button'>Регистрироватся</button>
                     </div>
                     <div className="form__redirect">
                 <Link to={'/'} className='form__link'>Войти на сайт </Link>
